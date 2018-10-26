c5a5f1f28cc21b917184b2608ead6c03e5402360
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-10-26 12:40:21 +0200

    [feat] - add map on main page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,10 +9,22 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
+import Map from '../components/Map'
 import { MonoText } from '../components/StyledText';
 
+// A placeholder until we get our own location
+const region = {
+  latitude: 45.757351,
+  longitude: 4.833747,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+}
+
 export default class HomeScreen extends React.Component {
+  state = {
+    region: null,
+    pharmacies: []
+  }
   static navigationOptions = {
     header: null,
   };
@@ -20,46 +32,10 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+        <Map
+          region={region}
+          places={this.state.pharmacies}
+        />
       </View>
     );
   }
